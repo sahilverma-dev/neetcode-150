13661efcd69e42d0e25f923073833ee7834cc4f8
Sahil Verma <domainsahil007@gmail.com>
2024-07-08 22:58:34 +0530

    min stack

---
+++ Stack/min-stack.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/min-stack/
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            if self.stack[-1] == self.min_stack[-1]:
+                self.min_stack.pop()
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        return None
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
+
+
+# Usage
+minStack = MinStack()
+
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin())  # return -3
+minStack.pop()
+print(minStack.top())  # return 0
+print(minStack.getMin())  # return -2
